43c22ea598afa82f6bceea9920b5a42585cc701b
Spider <silentwalk15@gmail.com>
2016-04-25 08:58:29 +0200

    Initial Commit

---
+++ IIT_2nd_Assignment_admur13/src/IIT_2nd_Assignment_admur13/ViewModels/Manage/ConfigureTwoFactorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc.Rendering;
+
+namespace IIT_2nd_Assignment_admur13.ViewModels.Manage
+{
+    public class ConfigureTwoFactorViewModel
+    {
+        public string SelectedProvider { get; set; }
+
+        public ICollection<SelectListItem> Providers { get; set; }
+    }
+}
---
+++ IIT_2nd_Assignment_admur13/src/IIT_2nd_Assignment_admur13/ViewModels/Manage/RemoveLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IIT_2nd_Assignment_admur13.ViewModels.Manage
+{
+    public class RemoveLoginViewModel
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+    }
+}
