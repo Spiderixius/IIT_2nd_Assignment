eafebcedaca6d07d45bcb0ce5fc666dc0e588860
Spider <silentwalk15@gmail.com>
2016-04-29 08:56:40 +0200

    Added view model for Gallery

---
+++ IIT_2nd_Assignment_admur13/src/IIT_2nd_Assignment_admur13/ViewModels/Gallery/ImageUploadViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IIT_2nd_Assignment_admur13.ViewModels.Gallery
+{
+    public class ImageUploadViewModel
+    {
+    }
+}
