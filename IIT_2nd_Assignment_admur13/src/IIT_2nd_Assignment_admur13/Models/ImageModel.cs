6f156e4cf1ef702289bf8836c5476252d1e961e9
Spider <silentwalk15@gmail.com>
2016-04-29 08:55:03 +0200

    Added pages for Gallery

---
+++ IIT_2nd_Assignment_admur13/src/IIT_2nd_Assignment_admur13/Models/ImageModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IIT_2nd_Assignment_admur13.Models
+{
+    public class ImageModel
+    {
+    }
+}
