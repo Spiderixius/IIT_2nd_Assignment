dc9bbfdb57c569a7660ecbd97fee27e9f361c4de
Spider <silentwalk15@gmail.com>
2016-04-30 12:42:19 +0200

    Image uploading added and seems like it is saving it to the database.

--- IIT_2nd_Assignment_admur13/src/IIT_2nd_Assignment_admur13/Controllers/GalleryController.cs
+++ IIT_2nd_Assignment_admur13/src/IIT_2nd_Assignment_admur13/Controllers/GalleryController.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IIT_2nd_Assignment_admur13.Models;
+using IIT_2nd_Assignment_admur13.ViewModels.Gallery;
 using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Mvc;
+using System.IO;
+using Microsoft.Net.Http.Headers;
+using System.Security.Claims;
+using Microsoft.AspNet.Http;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -29,6 +34,7 @@ namespace IIT_2nd_Assignment_admur13.Controllers
 
         }
 
+        // GET: /Gallery/UploadImage
         [HttpGet]
         [Authorize]
         public IActionResult ShowUpload()
@@ -36,6 +42,93 @@ namespace IIT_2nd_Assignment_admur13.Controllers
             return View("UploadImage");
         }
 
+        // POST: /Gallery/UploadImage
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public IActionResult UploadImage(ImageUploadViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var img = model.Image;
+
+                Byte[] imageBytes;
+                using (Stream _stream = img.OpenReadStream())
+                {
+                    imageBytes = ReadFully(_stream, (int) _stream.Length);
+                }
+
+                ImageModel image = new ImageModel
+                {
+                    Title = model.Title,
+                    ImageBytes = imageBytes,
+                    ImageType = img.ContentType
+                };
+                using (_appDbContext)
+                {
+                    _appDbContext.Images.Add(image);
+                    _appDbContext.SaveChanges();
+                }
+                return RedirectToAction("Gallery");
+            }
+            else {
+                ModelState.AddModelError(string.Empty, "Error uploading image.");
+                return View(model);
+            }
+        }
+
+
+        /// <summary>
+        ///  This section is for code copied from an external source. The copied code is appropriately marked with an URL above it. 
+        ///  Comments are by the author of the code snippet.
+        /// </summary>
+
+        // Code copied from http://www.yoda.arachsys.com/csharp/readbinary.html and yes I understand what is going on!
+
+        public static byte[] ReadFully(Stream stream, int initialLength)
+        {
+            // If we've been passed an unhelpful initial length, just
+            // use 32K.
+            if (initialLength < 1)
+            {
+                initialLength = 32768;
+            }
+
+            byte[] buffer = new byte[initialLength];
+            int read = 0;
+
+            int chunk;
+            while ((chunk = stream.Read(buffer, read, buffer.Length - read)) > 0)
+            {
+                read += chunk;
+
+                // If we've reached the end of our buffer, check to see if there's
+                // any more information
+                if (read == buffer.Length)
+                {
+                    int nextByte = stream.ReadByte();
+
+                    // End of stream? If so, we're done
+                    if (nextByte == -1)
+                    {
+                        return buffer;
+                    }
+
+                    // Nope. Resize the buffer, put in the byte we've just
+                    // read, and continue
+                    byte[] newBuffer = new byte[buffer.Length * 2];
+                    Array.Copy(buffer, newBuffer, buffer.Length);
+                    newBuffer[read] = (byte)nextByte;
+                    buffer = newBuffer;
+                    read++;
+                }
+            }
+            // Buffer is now too big. Shrink it.
+            byte[] ret = new byte[read];
+            Array.Copy(buffer, ret, read);
+            return ret;
+        }
 
     }
+
 }
